235bfec90a2c670a171e44509b57123d7adb718a
root <root@DESKTOP-456G5QK>
2023-12-04 15:24:15 +0900

    일치율 계산 추가

--- SyncAnalyzer/lib/syncLib.py
+++ SyncAnalyzer/lib/syncLib.py
@@ -1,15 +1,21 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
+import numpy as np, math
 
 def calculate_similarity(sentence1: str, sentence2: str):
+    
+    print("Calculating Similarity")
+    print(sentence1)
+    print(sentence2)
+    
     # Tokenize and convert sentences to vectors
     vectorizer = CountVectorizer().fit_transform([sentence1, sentence2])
     vectors = vectorizer.toarray()
 
     # Calculate cosine similarity
     similarity = cosine_similarity([vectors[0]], [vectors[1]])[0, 0]
-
-    return similarity
+    print(similarity, "\n")
+    
+    return round(similarity, 3)
 
 
--- SyncAnalyzer/syncAnalyzer.py
+++ SyncAnalyzer/syncAnalyzer.py
@@ -1,26 +1,38 @@
+from operator import index
 import pandas as pd
+import numpy as np
+from lib.syncLib import calculate_similarity as cs
 
 # Big Kinds는 '일자', 현재는 테스트 전용
-DATE_COLUMN: str = 'DATE'
+DATE_COLUMN: str = '일자'
 
 # YEAR-MONTH-DAY 중 DAY를 제거한다
 def changeDate(date):
     return str(date)[0:6]
 
+def formatKeywords(x: pd.Series):
+    array = [val for val in x.values if not pd.isnull(val)]
+    return str(''.join(array))
+
 # 뉴스 데이터를 월 기준으로 재구조화
 def initNews(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-    df[DATE_COLUMN] = df[DATE_COLUMN].apply(changeDate)
-    return df.groupby(by=['DATE'])['KEYWORD'].apply(lambda x: ' '.join(x)).reset_index()
+    df = pd.read_csv(path, index_col = 0)
+    df['일자'] = df['일자'].apply(changeDate)
+    return df.groupby(by=['일자'])['특성추출(가중치순 상위 50개)'].apply(formatKeywords).reset_index()
     
 
 def main():
     
     print("# Absolute path of keyword file")
-    df_keywords = pd.read_csv(input())
+    df_keywords = pd.read_csv(input(), index_col = 0)
+    keywords = df_keywords.set_index('DATE').to_dict()['KEYWORDS']
+    print(keywords[202301])
+    
     
     print("\n#Absolute path of news file")
     df_news = initNews(input())
+    df_news['일치율'] = df_news['일자'].apply(lambda x: cs(keywords[int(x)].replace(",", " "), ''.join(df_news['특성추출(가중치순 상위 50개)'].values).replace(",", " ")))
+    print(df_news['일치율'].to_string())
     
     # TODO : 반복하며 Cosine Similarity 계산하기.
 
