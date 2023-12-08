471449f8cfbf16c8d8a6bfa494df2b0bdeee3f66
rlaehd62 <root@DESKTOP-456G5QK>
2023-12-08 20:14:43 +0900

    방식 변경, 재커밋

--- SyncAnalyzer/lib/syncLib.py
+++ SyncAnalyzer/lib/syncLib.py
@@ -8,12 +8,9 @@ def calculate_similarity(sentence1: str, sentence2: str):
     print(sentence1)
     print(sentence2)
     
-    # Tokenize and convert sentences to vectors
-    
     vectors = TfidfVectorizer().fit_transform([sentence1, sentence2])
-
-    # Calculate cosine similarity
     similarity = cosine_similarity(vectors[0:1], vectors[1:2])[0][0]
+    
     print(similarity, "\n")
     return similarity
 
