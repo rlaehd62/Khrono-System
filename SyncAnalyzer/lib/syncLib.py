b0a0259c1dfe765d73d3fa60c682f1f2b1679cf0
rlaehd62 <root@DESKTOP-456G5QK>
2023-12-08 20:14:17 +0900

    방식 변경

--- SyncAnalyzer/lib/syncLib.py
+++ SyncAnalyzer/lib/syncLib.py
@@ -1,4 +1,4 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np, math
 
@@ -9,13 +9,11 @@ def calculate_similarity(sentence1: str, sentence2: str):
     print(sentence2)
     
     # Tokenize and convert sentences to vectors
-    vectorizer = CountVectorizer().fit_transform([sentence1, sentence2])
-    vectors = vectorizer.toarray()
+    
+    vectors = TfidfVectorizer().fit_transform([sentence1, sentence2])
 
     # Calculate cosine similarity
-    similarity = cosine_similarity([vectors[0]], [vectors[1]])[0, 0]
+    similarity = cosine_similarity(vectors[0:1], vectors[1:2])[0][0]
     print(similarity, "\n")
-    
     return similarity
-
 
