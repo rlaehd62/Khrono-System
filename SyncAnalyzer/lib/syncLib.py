a68593aa019686b4d525af074b7ef92fb8ba1c26
rlaehd62 <rlaehd62@github.comm>
2023-12-10 20:47:45 +0900

    자카드 방식으로 재변경

--- SyncAnalyzer/lib/syncLib.py
+++ SyncAnalyzer/lib/syncLib.py
@@ -1,13 +1,10 @@
-from sentence_transformers import SentenceTransformer, util
-
-
-model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS')
 def calculate_similarity(sentence1: str, sentence2: str):
     
-    embeddings1 = model.encode([sentence1], convert_to_tensor=True)
-    embeddings2 = model.encode([sentence2], convert_to_tensor=True)
+    set1 = set(sentence1.split())
+    set2 = set(sentence2.split())
     
-    cosine_scores = util.cos_sim(embeddings1, embeddings2)
-    print(f"{sentence1} vs {sentence2} : {cosine_scores[0][0]:0.4f}")
+    intersection = set1 & set2
+    union = set1 | set2
     
-    return cosine_scores[0][0]
+    print(intersection)
+    return len(intersection) / len(union) if len(union) != 0 else 0
